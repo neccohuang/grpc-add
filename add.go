0f06abf44f1e851bb577fce6c3fba71b82675bc4
Peter <necco.huang@goldenf.co>
2024-07-05 11:28:10 +0800

    init

---
+++ add.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/zeromicro/zero-contrib/zrpc/registry/consul"
+
+	"github.com/neccohuang/grpc-add/add"
+	"github.com/neccohuang/grpc-add/internal/config"
+	"github.com/neccohuang/grpc-add/internal/server"
+	"github.com/neccohuang/grpc-add/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var configFile = flag.String("f", "etc/add.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		add.RegisterAddServer(grpcServer, server.NewAddServer(ctx))
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	_ = consul.RegisterService(c.ListenOn, c.Consul)
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
